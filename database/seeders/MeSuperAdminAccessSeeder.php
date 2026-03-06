8cea314a6451076ef5783783e728beb6a95b34a7
Kalkidan <dersokalkidan@gmail.com>
2026-03-06 13:59:04 +0300

    fix: correct M&E permission query patterns in MeSuperAdminAccessSeeder

--- database/seeders/MeSuperAdminAccessSeeder.php
+++ database/seeders/MeSuperAdminAccessSeeder.php
@@ -20,9 +20,13 @@ class MeSuperAdminAccessSeeder extends Seeder
 
         $mePermissions = Permission::query()
             ->where(function ($query) {
-                $query->where('name', 'like', '%:Me%')
-                    ->orWhere('name', 'like', 'page_Me%')
-                    ->orWhere('name', 'like', 'page_%WeeklyReport%');
+                // Shield generates permissions in lowercase underscore format e.g. view_me_indicator
+                $query->where('name', 'like', '%_me_%')        // resource actions: view_me_indicator, create_me_project, etc.
+                    ->orWhere('name', 'like', 'view_any_me_%') // viewAny_ prefix variant
+                    ->orWhere('name', 'like', 'page_MeDashboard')       // MeDashboard page
+                    ->orWhere('name', 'like', 'page_Me%')               // any other Me* pages
+                    ->orWhere('name', 'like', 'page_%WeeklyReport%')    // ImportWeeklyReport page
+                    ->orWhere('name', 'like', 'page_%ImportWeekly%');   // alternate naming
             })
             ->get();
 
