7734d988f8880c9ff085e84f9a65b9a7e1ec9a01
Kalkidan <dersokalkidan@gmail.com>
2026-03-14 13:04:04 +0300

    remove recruitment module

--- resources/views/applications/show.blade.php
+++
@@ -1,7 +0,0 @@
-<h1 class="text-3xl font-bold mb-4">{{ $job->position }}</h1>
-
-@if(session('success'))
-<div class="mb-6 p-4 bg-green-500 text-white rounded-lg">
-    {{ session('success') }}
-</div>
-@endif
