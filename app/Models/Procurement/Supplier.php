108199dc553919d1ff244bbec26de5bef762a6a4
Kalkidan <dersokalkidan@gmail.com>
2026-03-27 21:58:02 +0300

    fix: add missing PaymentTerm import in Supplier model

--- app/Models/Procurement/Supplier.php
+++ app/Models/Procurement/Supplier.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\PaymentTerm;
 
 class Supplier extends Authenticatable
 {
