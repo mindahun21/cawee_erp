a44e2f78d5a52dc93d60a7283fa21d3d4ee1285b
mindahun21 <mndahun0@gmail.com>
2026-04-06 19:20:04 +0300

    restore: permision and role changes restored

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -1,11 +1,10 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Permission\Models\Permission as SpatiePermission;
 
 class Permission extends SpatiePermission
 {
-    use HasFactory, HasUuids;
+    use HasFactory;
 }
